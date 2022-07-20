99dadb348dd247ae998fe1b6df4ef55afea31db5
WilliamAmponsah <91913535+WilliamAmponsah@users.noreply.github.com>
2022-07-19 20:11:42 -0400

    removed the tabs and added a navbar

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RewardsComponent } from './rewards/rewards.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'logout', component: LogoutComponent},
+  {path: 'rewards', component: RewardsComponent},
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { RewardsComponent } from './rewards/rewards.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
@@ -34,6 +35,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     MatButtonModule,
     MatCardModule,
     MatProgressBarModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
